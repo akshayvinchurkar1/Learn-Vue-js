5a23ed98f1a9af55efc595548ce304d5a26e999e
akshay vinchurkar <akshayv@vcsbim.com>
2017-10-30 09:44:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+class Post {
+  constructor(name, title, year) {
+    this.name = name;
+    this.title = title;
+    this.year = year;  
+  }
+}
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    postlist: [
+        new Post (
+        'Parth Bhavasr', 
+        'the favoirate one', 
+        '2010'
+        ),
+        new Post (
+        'Chetan Aamle', 
+        'the favoirate two', 
+        '2016'
+        ),
+        new Post (
+        'Rutika dabholkar', 
+        'the favoirate three', 
+        '2018'
+        ),
+        new Post (
+        'Madhav Sarkar', 
+        'Coral Sans', 
+        '2019'
+        )
+    ]
+  },
+  methods: {
+    getthename: function() {
+      console.log(this.name);
+    }
+  }
+});
