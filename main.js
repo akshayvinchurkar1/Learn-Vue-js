23cefdf8a4d588cc122d8ad79efc0ee8dde3bb7c
akshay vinchurkar <akshayv@vcsbim.com>
2017-10-31 08:45:29 +0530

    get the current target element implimented

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ class Post {
   constructor(name, title, year) {
     this.name = name;
     this.title = title;
-    this.year = year;  
+    this.year = year;
   }
 }
 
@@ -11,30 +11,30 @@ var app = new Vue({
   data: {
     postlist: [
         new Post (
-        'Parth Bhavasr', 
-        'the favoirate one', 
+        'Parth Bhavasr',
+        'the favoirate one',
         '2010'
         ),
         new Post (
-        'Chetan Aamle', 
-        'the favoirate two', 
+        'Chetan Aamle',
+        'the favoirate two',
         '2016'
         ),
         new Post (
-        'Rutika dabholkar', 
-        'the favoirate three', 
+        'Rutika dabholkar',
+        'the favoirate three',
         '2018'
         ),
         new Post (
-        'Madhav Sarkar', 
-        'Coral Sans', 
+        'Madhav Sarkar',
+        'Coral Sans',
         '2019'
         )
     ]
   },
   methods: {
-    getthename: function() {
-      console.log(this.name);
+    getthename: function(e) {
+      console.log(e.currentTarget);
     }
   }
 });
